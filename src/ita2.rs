0336f683f7f9a33107dc98499aa6918504417f11
dthelegend <me@daudi.dev>
2024-06-01 11:31:14 +0100

    Add Decoder boilerplate

--- src/ita2.rs
+++ src/ita2.rs
@@ -1,3 +1,24 @@
-struct Decoder {
-    input_S
+struct Decoder<T>
+where
+    T: Iterator<Item = u8>,
+{
+    figure_shift: bool,
+    source: T
+}
+
+impl <T : Iterator<Item = u8>> Iterator for Decoder<T> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(v) = self.source.next() {
+            if self.figure_shift {
+                match v {
+                    _ => []
+                };
+            } else {
+                match v { _ => {} }
+            }
+        }
+        None
+    }
 }
