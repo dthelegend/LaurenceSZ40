cb23cc53d674b6381fe166e26b24961f1d3490db
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-06-01 17:45:18 +0100

    Display demo

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ mod lorenz;
 
 use arduino_hal::spi;
 use lorenz::LorenzWheel;
+use max7219::MAX7219;
+use max7219_canvas::{layer::CanvasLayer, DisplayCanvas};
 use panic_halt as _;
 
 use smart_leds::{SmartLedsWrite, RGB8};
@@ -17,49 +19,64 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let din = pins.d13.into_output();
+    let cs = pins.d12.into_output();
+    let clk = pins.d11.into_output();
 
-    let (spi, _) = spi::Spi::new(
-        dp.SPI,
-        pins.d52.into_output(),
-        pins.d51.into_output(),
-        pins.d50.into_pull_up_input(),
-        pins.d53.into_output(),
-        spi::Settings {
-            // clock: spi::SerialClockRate::OscfOver8,
-            ..Default::default()
-        },
-    );
+    let mut display = MAX7219::from_pins(1, din, cs, clk).unwrap();
 
-    let mut output_buffer = [0; 40 + (300 * 12)];
-    let mut data: [RGB8; 300] = [RGB8::default(); 300];
-    let empty: [RGB8; 300] = [RGB8::default(); 300];
-    let mut ws = Ws2812::new(spi, &mut output_buffer);
+    display.power_on().unwrap();
+    display.set_intensity(0, 0x0f).unwrap();
 
-    let mut lit = 0;
+    // Create display canvas
+
+    let mut canvas: DisplayCanvas<2 /* 2 layers */, 1 /* 1 display */> = DisplayCanvas::new();
+
+    // Create a static layer that never changes
+
+    let mut static_layer = CanvasLayer::new();
+
+    // Smiley face
+
+    static_layer.set_pixel(1, 3, true);
+    static_layer.set_pixel(1, 2, true);
+    static_layer.set_pixel(2, 1, true);
+    static_layer.set_pixel(3, 1, true);
+    static_layer.set_pixel(4, 1, true);
+    static_layer.set_pixel(5, 1, true);
+    static_layer.set_pixel(6, 2, true);
+    static_layer.set_pixel(6, 3, true);
+
+    static_layer.set_pixel(1, 6, true);
+    static_layer.set_pixel(2, 6, true);
+    static_layer.set_pixel(1, 5, true);
+    static_layer.set_pixel(2, 5, true);
+
+    static_layer.set_pixel(6, 6, true);
+    static_layer.set_pixel(5, 6, true);
+    static_layer.set_pixel(6, 5, true);
+    static_layer.set_pixel(5, 5, true);
+
+    // Create an animated layer
+
+    let mut animated_layer = CanvasLayer::new();
+    let mut animation_position: usize = 0;
 
     loop {
-        data[lit] = RGB8 {
-            r: 0x10,
-            g: 0x10,
-            b: 0x10,
-        };
+        // Update the animated layer
 
-        ws.write(data.iter().cloned()).unwrap();
+        animated_layer.clear();
+        animated_layer.set_pixel(animation_position, animation_position, true);
+
+        animation_position = (animation_position + 1) % 8;
+
+        // Update the display
+
+        canvas.update_layer(0, static_layer);
+        canvas.update_layer(1, animated_layer);
+
+        canvas.write_to_display(0, &mut display); // Display the canvas to the first display
+
         arduino_hal::delay_ms(100);
-        data = empty;
-        if lit == 299 {
-            lit = 0;
-        } else {
-            lit += 1;
-        }
     }
 }
